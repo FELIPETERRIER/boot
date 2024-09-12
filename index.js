f894f95aa289daafd90e1d2249158b5190f48a98
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2024-09-12 20:45:10 -0300

    colocando o gitignore

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("poe marcha")
+const getRelacao =   async (req, res) => {
+    const relacaoFuncao = {...req.body}
+    try {
+        const result = await pesMR.savePessoaModel(pessoa);
+
+        res.status(201).json(
+            {
+                dados: result
+            }
+        );
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            codigoErro: 500,
+            mensagem: error
+        });    }
+}
