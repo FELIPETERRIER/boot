645070a49856f11307df6ebc80bde393f412b283
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2025-03-02 10:14:10 -0300

    finalizado o erro da autenticação do usuario

--- models/loginModel.js
+++ models/loginModel.js
@@ -4,20 +4,16 @@ const login ={
 postLoginUsuarioModel: async (usuario) => {
     
        
-        const loginUsuario = Object.values(usuario);
-        //console.log(loginUsuario    )
-           
-      
-        const loginOkUsuario  = await b.query('SELECT cpf,password FROM usuarios')   ;
-        console.log(loginOkUsuario.rows)
-        console.log(loginUsuario)
-       
-       
-        if (loginOkUsuario.rows == loginUsuario) {
-           
-            console.log('certo miseravi!')
-        }else{
-            console.log('energumeno')
+        const loginUsuario = Object.values(usuario);      
+        const loginOkUsuario  = await db.query('SELECT cpf,password FROM usuarios');       
+        const encontrado = loginOkUsuario.rows.some(row => 
+            Object.values(row).every((valor, index) => valor === loginUsuario[index])
+        );
+        
+        if (encontrado) {
+            console.log('Login bem-sucedido!');
+        } else {
+            console.log('CPF ou senha incorretos.');
         }
     }
 }
