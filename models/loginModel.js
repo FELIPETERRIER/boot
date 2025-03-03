a7d8dba98ac4042ea845ae4974769990e6ffd0f5
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2025-03-02 21:07:36 -0300

    implementação jwt

--- models/loginModel.js
+++ models/loginModel.js
@@ -1,17 +1,20 @@
-const db = require('../dataBase/dataBase')
+const db = require('../dataBase/dataBase');
+const jwt = require('jsonwebtoken');
 const login ={
 
 postLoginUsuarioModel: async (usuario) => {
     
        
         const loginUsuario = Object.values(usuario);      
-        const loginOkUsuario  = await db.query('SELECT cpf,password FROM usuarios');       
+        const loginOkUsuario  = await db.query('SELECT cpf,password,id FROM usuarios');       
         const encontrado = loginOkUsuario.rows.some(row => 
             Object.values(row).every((valor, index) => valor === loginUsuario[index])
-        );
+        );  
         
         if (encontrado) {
-            console.log('Login bem-sucedido!');
+            const userID = loginUsuario[2];            
+            console.log(userID);
+            //jwt.sign(userID),SECRET,
         } else {
             console.log('CPF ou senha incorretos.');
         }
